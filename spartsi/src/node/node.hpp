64dab1c80347f451d5726e575477531d241bf8cb
epstacbiz2@gmail.com <epstacbiz2@gmail.com>
2016-02-22 15:23:46 +0100

    One may create node only by the usage of node.spawn_child or node::spawn_root

--- spartsi/src/node/node.hpp
+++ spartsi/src/node/node.hpp
@@ -8,29 +8,95 @@
 namespace spartsi {
 
     template<typename Str>
-    struct node {
+    class node {
+    public:
         using shared = std::shared_ptr<node<Str>>;
         using str_t = Str;
+        using attributes_t = std::unordered_map<str_t, str_t>;
+        using children_t = std::unordered_map<str_t, node::shared>;
+    private:
+        node(
+            view_of<str_t> name,
+            view_of<str_t> version,
+            view_of<str_t> comment
+        ): name(name), version(version), comment(comment)
+        {}
 
+        node(
+            view_of<str_t> name,
+            view_of<str_t> version,
+            view_of<str_t> comment,
+            view_of<attributes_t> attrs
+        ): name(name), version(version), comment(comment), attributes(attrs)
+        {}
+
+        node(
+            const node *parent,
+            view_of<str_t> name,
+            view_of<str_t> comment
+        ):
+            parent(parent),
+            name(name),
+            version(parent->version),
+            comment(comment)
+        {}
+
+        node(
+            const node *parent,
+            view_of<str_t> name,
+            view_of<str_t> comment,
+            view_of<attributes_t> attrs
+        ):
+            parent(parent),
+            name(name),
+            version(parent->version),
+            comment(comment),
+            attributes(attrs)
+        {}
+
+    public:
         const node *parent;
 
         str_t name, version, comment;
-        std::unordered_map<str_t, str_t> attributes;
-        std::unordered_map<str_t, node::shared> children;
+        attributes_t attributes;
+        children_t children;
+
+        node::shared spawn_child(
+            view_of<str_t> name,
+            view_of<str_t> comment
+        ){
+            auto result = shared(new node<Str>(this, name, comment));
+            this->children[name] = result;
+            return result;
+        }
+
+        node::shared spawn_child(
+            view_of<str_t> name,
+            view_of<str_t> comment,
+            view_of<attributes_t> attrs
+        ){
+            auto result = shared(new node<Str>(this, name, comment, attrs));
+            this->children[name] = result;
+            return result;
+        }
+
+
+
+        static node::shared spawn_root(
+            view_of<str_t> name,
+            view_of<str_t> version,
+            view_of<str_t> comment
+        ){
+            return shared(new node<Str>(name, version, comment));
+        }
+
+        static node::shared spawn_root(
+            view_of<str_t> name,
+            view_of<str_t> version,
+            view_of<str_t> comment,
+            view_of<attributes_t> attrs
+        ){
+            return shared(new node<Str>(name, version, comment, attrs));
+        }
     };
-
-    template<typename Str, typename StrView = view_of<Str>>
-    typename node<Str>::shared create_node(StrView name, StrView version, StrView comment = ""){
-        using node_t = node<Str>:
-        auto result = std::shared_ptr<node_t>(new node_t{ nullptr, name, version, comment });
-        return std::make_shared<node<Str>>(nullptr, name, version, comment);
-    }
-
-    template<typename Node, typename StrView = view_of<typename Node::str_t>>
-    Node create_node(view_of<Node> parent, StrView name, StrView comment){
-        using node_t = Node;
-        return std::make_shared<Node>(&parent, name, parent.version, comment);
-    }
-
-
 }
