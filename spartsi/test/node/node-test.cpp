dde9e5428b423901a5bf2e048a3af5ae31132dc1
epstacbiz2@gmail.com <epstacbiz2@gmail.com>
2016-02-22 15:23:57 +0100

    Well. These root's are fine.

--- spartsi/test/node/node-test.cpp
+++ spartsi/test/node/node-test.cpp
@@ -5,19 +5,45 @@
 using std::string;
 
 TEST_CASE("Creation of simple nodes", "[spartsi::node]") {
-    auto name = "The sample object";
-    auto version = "1.0";
-    auto comment = "sample comment";
+    using node = spartsi::node<string>;
 
-    auto node = spartsi::create_node<string>(name, version, comment);
+    SECTION("children-less, attribute-less") {
+        auto name = "The sample object";
+        auto version = "1.0";
+        auto comment = "sample comment";
 
-    SECTION("Node is not null") {
-        REQUIRE(node != nullptr);
+        auto node = node::spawn_root(name, version, comment);
+
+        SECTION("Node is not null") {
+            REQUIRE(node != nullptr);
+        }
+
+        SECTION("Infos are properly set") {
+            REQUIRE(node->name == name);
+            REQUIRE(node->version == version);
+            REQUIRE(node->comment == comment);
+        }
     }
 
-    SECTION("Infos are properly set") {
-        REQUIRE(node->name == name);
-        REQUIRE(node->version == version);
-        REQUIRE(node->comment == comment);
+    SECTION("node with attributes (lets invoke the second constructor)"){
+        auto name = "Some name";
+        auto version = "2.0";
+        auto comment = "<insert hate here>";
+        auto attr_key = "foo";
+        auto attr_val = "bar";
+        node::attributes_t attrs = {{attr_key, attr_val}};
+
+        auto node = node::spawn_root(name, version, comment, attrs);
+
+        SECTION("Node is not null") {
+            REQUIRE(node != nullptr);
+        }
+
+        SECTION("Infos are properly set") {
+            REQUIRE(node->name == name);
+            REQUIRE(node->version == version);
+            REQUIRE(node->comment == comment);
+            REQUIRE(node->attributes[attr_key] == attr_val);
+        }
     }
 }
