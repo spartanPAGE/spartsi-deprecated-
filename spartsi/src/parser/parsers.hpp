1d4467c7cda62e5416208c611d1a41dc27adcb8f
epstacbiz2@gmail.com <epstacbiz2@gmail.com>
2016-02-23 12:59:18 +0100

    Added parsers skeleton

---
+++ spartsi/src/parser/parsers.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <functional>
+
+#include "core/view_of.hpp"
+
+namespace spartsi{
+
+    using regular_expression_t = std::string;
+    using parsing_func_t = std::function<void(view_of<std::string>)>;
+
+    using parsers = std::unordered_map<
+        regular_expression_t,
+        parsing_func_t
+    >;
+}
