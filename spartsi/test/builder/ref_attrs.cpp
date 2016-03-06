f511c9e9f1f69b7ddcce8747d662ef8841760bc1
epstacbiz2@gmail.com <epstacbiz2@gmail.com>
2016-03-06 21:00:06 +0100

    ref_ -> req_

--- spartsi/test/builder/ref_attrs.cpp
+++ spartsi/test/builder/ref_attrs.cpp
@@ -7,17 +7,17 @@ TEST_CASE("Ref attrs", "[spartsi::builder]") {
     auto tree = build()
         .treestructinfo("2.0").name("ref attrs")
             ("foo")
-            .ref_attr("1")
+            .req_attr("1")
 
             .node("1st node")
-                .ref_attr("2")
+                .req_attr("2")
             .end_node()
 
             .node("2nd node")
-                .ref_attr("3")
+                .req_attr("3")
 
                 .node("2nd 1st node")
-                    .ref_attr("4")
+                    .req_attr("4")
                 .end_node()
             .end_node()
         .end_tree()
