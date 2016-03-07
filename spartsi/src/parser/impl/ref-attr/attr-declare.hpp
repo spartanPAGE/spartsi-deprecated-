7f9e7574121a6327c619f71433bbe6e1bf8e0c25
Patryk Wertka <spartanpageagain@gmail.com>
2016-03-07 09:51:58 +0100

    added ref-attr related parsers

---
+++ spartsi/src/parser/impl/ref-attr/attr-declare.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "langspec/langspec.hpp"
+#include "builder/tree-builder.hpp"
+
+#include "parser/impl/util.hpp"
+
+namespace spartsi {
+    namespace parser {
+        template<typename Str>
+        bool attr_declare(builder::tree_builder &bld, langspec<Str> spec, const Str &line) {
+            using namespace util;
+            std::smatch m;
+
+            std::string pattern = spec.ref_attr.name + " " + capture;
+
+            if(std::regex_match(line, m, weak_reg(pattern))) {
+                bld.req_attr(extract_captured(m[1]));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
