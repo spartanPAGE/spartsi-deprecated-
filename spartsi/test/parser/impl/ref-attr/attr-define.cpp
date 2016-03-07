5510f94502970462aedbd256c4fe337d950bb9d8
Patryk Wertka <spartanpageagain@gmail.com>
2016-03-07 10:10:44 +0100

    Added parser::attr_define test suite

---
+++ spartsi/test/parser/impl/ref-attr/attr-define.cpp
@@ -0,0 +1,24 @@
+#include "catch/catch.hpp"
+
+#include "parser/impl/ref-attr/attr-define.hpp"
+#include "langspec/default.hpp"
+using namespace std;
+
+TEST_CASE("define attr", "[spartsi::parser::attr_define") {
+    auto spec = spartsi::default_::langspec;
+
+    auto bld = spartsi::builder::build();
+    bld.treestructinfo("");
+    bld.req_attr("foo attr");
+    bld.end_tree();
+
+    SECTION("good") {
+        REQUIRE(spartsi::parser::attr_define(bld, spec, std::string("ref attr foo attr \"val\"")));
+        REQUIRE(bld.get()->attributes.count("foo attr"));
+        REQUIRE(bld.get()->attributes.at("foo attr").first == "val");
+
+        SECTION("bad") {
+            REQUIRE_THROWS(spartsi::parser::attr_define(bld, spec, std::string("ref attr unknown \"val\"")));
+        }
+    }
+}
